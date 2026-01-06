009e27d89852b9a8e6fb36354bf1ef419d48a7a6
Eduardo Brito Labrada <eblabrada@gmail.com>
2026-01-06 02:15:31 -0500

    add basic generator

---
+++ src/generator.cpp
@@ -0,0 +1,40 @@
+#include "utils/testlib.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    registerGen(argc, argv, 1);
+    
+    string mode = argv[1];
+    
+    int V, K;
+    if (mode == "small") {
+        V = rnd.next(1, 6);
+        K = rnd.next(1, max(1, V - 1));
+    } else if (mode == "medium") {
+        V = rnd.next(1, 10);
+        K = rnd.next(1, max(1, V - 1)); 
+    } else if (mode == "big") {
+        V = rnd.next(1, 1000);
+        K = rnd.next(1, max(1, V - 1)); 
+    } else {
+        assert(false);
+    }
+
+    vector mat(V + 1, vector(V + 1, 0.0L));
+    for (int i = 0; i < V; i++) {
+        for (int j = i + 1; j < V; j++) {
+            mat[i][j] = mat[j][i] = rnd.next(1e9);
+        }
+    }
+
+    println(V, K);
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            cout << setprecision(12) << fixed << mat[i][j] << " \n"[j == V - 1];
+        }
+    }
+}
