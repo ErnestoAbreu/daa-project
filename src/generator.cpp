50d93668fc23549372b333182260063afb5f0c06
ErnestoAbreu <ernestoabreup@gmail.com>
2026-01-08 10:27:26 -0500

    modify generator to integer output

--- src/generator.cpp
+++ src/generator.cpp
@@ -6,30 +6,34 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     registerGen(argc, argv, 1);
-    
+
     string mode = argv[1];
-    
+
     int V, K;
     if (mode == "fixed") {
         V = atoi(argv[2]);
         K = atoi(argv[3]);
-    } else if (mode == "small") {
+    }
+    else if (mode == "small") {
         V = rnd.next(1, 6);
         K = rnd.next(1, max(1, V - 1));
-    } else if (mode == "medium") {
+    }
+    else if (mode == "medium") {
         V = rnd.next(1, 10);
-        K = rnd.next(1, max(1, V - 1)); 
-    } else if (mode == "big") {
+        K = rnd.next(1, max(1, V - 1));
+    }
+    else if (mode == "big") {
         V = rnd.next(1, 1000);
-        K = rnd.next(1, max(1, V - 1)); 
-    } else {
+        K = rnd.next(1, max(1, V - 1));
+    }
+    else {
         assert(false);
     }
 
     vector mat(V + 1, vector(V + 1, 0.0L));
     for (int i = 0; i < V; i++) {
         for (int j = i + 1; j < V; j++) {
-            mat[i][j] = mat[j][i] = rnd.next(1e9);
+            mat[i][j] = mat[j][i] = rnd.next(100);
         }
     }
 
@@ -37,7 +41,7 @@ int main(int argc, char* argv[])
 
     for (int i = 0; i < V; i++) {
         for (int j = 0; j < V; j++) {
-            cout << setprecision(12) << fixed << mat[i][j] << " \n"[j == V - 1];
+            cout << mat[i][j] << " \n"[j == V - 1];
         }
     }
 }
